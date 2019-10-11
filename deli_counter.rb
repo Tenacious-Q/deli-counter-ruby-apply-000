b7e58c49af136bf5da7bda481b6e1d59cf99ffde
dazzling-node-4320 <mikequigley@quigleypublishing.com>
2019-10-11 12:42:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,17 @@ katz_deli = ["Larry", "Curly", "Moe"]
 
 
 def line(array)
-  if array.empty?
-    puts "The line is currently empty."
-    end
-    puts "The line is currently: #{next_line.join(" ")}"
-
-  else
+  if array.length >= 1
     next_line = []
     counter = 1
     array.each do |name|
       next_line << ("#{counter}. #{name}")
       counter += 1
+    end
+    puts "The line is currently: #{next_line.join(" ")}"
 
+  else
+    puts "The line is currently empty."
   end
 
 end
